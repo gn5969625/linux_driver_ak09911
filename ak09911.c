e5d859debe9b3822803b363bb03198d212a4e597
Kevin <gn5969625@gmail.com>
2018-09-05 17:29:34 +0800

    Continue porint drive and add input subsystem

--- ak09911.c
+++ ak09911.c
@@ -14,16 +14,25 @@
 #include <linux/delay.h>
 #include "ak09911.h"
 #include <linux/sysfs.h>
-#define DRV_VERSION "V1.0"
+#define DRV_VERSION "V2.0"
+
+#define AKM_09911_MAX_RANGE 8190
 static struct i2c_client *ak09911_client;
+typedef struct akm09911_data {
+       struct input_dev *input_dev;
+       struct work_struct work;
+       unsigned char asa[3]; //get Sensitivity Adjustment Values from fuse rom
+       struct mutex lock;
+       unsigned char mode;
+}akm09911_data;
+akm09911_data akm_data;
 
 static ssize_t ak09911_show(struct device *dev,struct device_attribute *attr, char *buf)
 {
-	return sprintf(buf,"CH0_data = %d, CH1_data = %d, Prox_data = %d \n",CH0_data, CH1_data, Prox_data);
+	return 0;
 }
 static ssize_t ak09911_store(struct device *dev,struct device_attribute *attr, const char *buf, size_t count)
 {
-	int ret;
 	return 0;
 }
 static DEVICE_ATTR(data, 0644 , ak09911_show, NULL);
@@ -35,25 +44,60 @@ static struct attribute_group mydrv_attr_group = {
     .name = "ak09911",
     .attrs = ak09911_attrs,
 };
+void set_akm09911_reset() {
+    
+}
+void set_akm09911_mode() {
+     int res;
+     akm_data.mode = AK09911_MODE_POWERDOWN;
+     rest = i2c_smbus_write_byte_data(ak09911_client,AK09911_REG_CNTL2,akm_data.mode);
+}
+void get_akm09911_asa(void) {
+     int res;
+     //enter fuse rom mode
+     res = i2c_smbus_write_byte_data(ak09911_client,AK09911_REG_CNTL2,AK09911_MODE_FUSE_ACCESS);
+     mdelay(100);
+     akm_data.asa[0] = i2c_smbus_read_byte_data(ak09911_client,AK09911_REG_ASAX);
+     akm_data.asa[1] = i2c_smbus_read_byte_data(ak09911_client,AK09911_REG_ASAY);
+     akm_data.asa[2] = i2c_smbus_read_byte_data(ak09911_client,AK09911_REG_ASAZ);
 
+     //return power dowm mode
+     res = res = i2c_smbus_write_byte_data(ak09911_client,AK09911_REG_CNTL2,AK09911_MODE_POWERDOWN);
+}
 static int ak09911_dev_init(void)
 {
 	int res;
 	printk("%s called\n", __func__);
-	res = i2c_smbus_write_byte_data(ak09911_client,, 0);
-	mdelay(12);
-	//read device id
-	res = i2c_smbus_read_byte_data(ak09911_client, REPEATED_BYTE | APDS9930_ID);
-	/*
-	if(res == ) {
-		printk("the device id:%x \n", res);
-	}
-	*/
-	return 0;
+	res = i2c_smbus_read_byte_data(ak09911_client, AK09911_REG_WIA1);
+        if(res < 0)
+	   return 0;
+        if(res == AK09911_WIA1_VALUE)
+            printk("%s,Campany ID:%x\n",__func__,res);
+        res = i2c_smbus_read_byte_data(ak09911_client, AK09911_REG_WIA2);
+	if(res < 0)
+           return 0;
+        if(res == AK09911_WIA2_VALUE)
+             printk("%s,Device ID:%x\n",__func__,res);
+
+        get_akm09911_asa();
+        set_mode();
+
+
+        akm_data.input_dev = input_alloc_device();
+        set_bit(EV_ABS, akm_data.input_dev->evbit);
+        /* x-axis magnetic */
+        input_set_abs_params(akm_data.input_dev, ABS_X, -AKM_09911_MAX_RANGE, AKM_09911_MAX_RANGE, 0, 0);
+        /* y-axis magnetic */
+        input_set_abs_params(akm_data.input_dev, ABS_Y, -AKM_09911_MAX_RANGE, AKM_09911_MAX_RANGE, 0, 0);
+        /* z-axis magnetic */
+        input_set_abs_params(akm_data.input_dev, ABS_Z, -AKM_09911_MAX_RANGE, AKM_09911_MAX_RANGE, 0, 0);
+        akm_data.input_dev->name = "akm09911";
+        //akm_data.input_dev->dev.parent = &client->dev;
+        res = input_register_device(akm_data.input_dev);
+
 }
 static int ak09911_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 {
-	//struct proc_dir_entry *file;
 	int ret;
 	dev_dbg(&i2c->dev, "%s\n", __func__);
 	if (!i2c_check_functionality(i2c->adapter, I2C_FUNC_I2C | I2C_FUNC_SMBUS_EMUL))
@@ -72,6 +116,19 @@ static int ak09911_remove(struct i2c_client *i2c)
 	return 0;
 }
 
+static struct file_operations akm09911_fops = {
+        .owner = THIS_MODULE,
+        .open = akm09911_open,
+        .release = akm09911_release,
+        .ioctl = akm09911_ioctl,
+};
+
+static struct miscdevice akm09911_device = {
+        .minor = MISC_DYNAMIC_MINOR,
+        .name = "akm09911_device",
+        .fops = &akm09911_fops,
+};
+
 static struct of_device_id ak09911_of_match[] = {
         { .compatible = "akm,ak09911" },
         { },
@@ -86,7 +143,7 @@ struct i2c_driver ak09911_driver = {
     .probe      = ak09911_probe,
     .remove     = ak09911_remove,
 };
-
+/*
 static int ak09911_init(void)
 {
 	return i2c_add_driver(&ak09911_driver);
@@ -97,11 +154,10 @@ static void ak09911_exit(void)
 	printk("exit ak09911 driver module");
 	i2c_del_driver(&ak09911_driver);
 }
-
 module_init(ak09911_init);
 module_exit(ak09911_exit);
-
-//module_i2c_driver(ak09911_driver);
+*/
+module_i2c_driver(ak09911_driver);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kevin.Shen");
 MODULE_DESCRIPTION("A i2c-ak09911 driver for testing module ");
--- ak09911.h
+++ ak09911.h
@@ -1,10 +1,14 @@
 #ifndef __AK09911_H_
-#define __AK09911_H
+#define __AK09911_H_
+
+//by CAD pin setting
+#define AK09911_I2C_CAD0_ADD            0x0C
+#define AK09911_I2C_CAD1_ADD            0x0D
+
 #define AK09911_REG_WIA1		0x00
 #define AK09911_REG_WIA2		0x01
-#define AK09911_WIA1_VALUE		0x48
-#define AK09911_WIA2_VALUE		0x05
-
+#define AK09911_REG_INFO1               0x02
+#define AK09911_REG_INFO2               0x03
 #define AK09911_REG_ST1			0x10
 #define AK09911_REG_HXL			0x11
 #define AK09911_REG_HXH			0x12
@@ -12,27 +16,31 @@
 #define AK09911_REG_HYH			0x14
 #define AK09911_REG_HZL			0x15
 #define AK09911_REG_HZH			0x16
-
+#define AK09911_REG_ST2                 0x18
+#define AK09911_REG_CNTL1		0x30 //dummy register
+#define AK09911_REG_CNTL2		0x31 //set device operation mode
+#define AK09911_REG_CNTL3		0x32 //software reset register
 #define AK09911_REG_ASAX		0x60
 #define AK09911_REG_ASAY		0x61
 #define AK09911_REG_ASAZ		0x62
 
-#define AK09911_REG_CNTL1		0x30
-#define AK09911_REG_CNTL2		0x31
-#define AK09911_REG_CNTL3		0x32
+#define AK09911_WIA1_VALUE		0x48
+#define AK09911_WIA2_VALUE		0x05
 
 #define AK09911_MODE_SNG_MEASURE	0x01
 #define AK09911_MODE_SELF_TEST		0x10
 #define AK09911_MODE_FUSE_ACCESS	0x1F
 #define AK09911_MODE_POWERDOWN		0x00
-#define AK09911_RESET_DATA		0x01
 
-#define AK09911_REG_CNTL1		0x30
-#define AK09911_REG_CNTL2		0x31
-#define AK09911_REG_CNTL3		0x32
+#define AK09911_ST1_HSM_MASK            0x80
+#define AK09911_ST1_DOR_MASK            0x02
+#define AK09911_ST1_DRDY_MASK           0x01
+
+#define AK09911_ST2_HOLF_MASK           0x08
+
+#define AK09911_RESET_DATA_MASK		0x01
 
 #define AK09911_RAW_TO_GAUSS(asa)	((((asa) + 128) * 6000) / 256)
-
 #define AK09911_MAX_CONVERSION_TIMEOUT_MS	500
 #define AK09911_CONVERSION_DONE_POLL_TIME_MS	10
 
